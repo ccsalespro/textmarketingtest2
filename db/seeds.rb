6b1b9d48b5d502204a1920c1add4620991d2e218
micahnico <micahnicodemus22@gmail.com>
2017-07-06 16:33:38 -0400

    try to fix permissions

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,37 +1,37 @@
 # Company Users Permissions
-CompanyPermission.create(name: "Invite Company Users")
-CompanyPermission.create(name: "View Company Users")
-CompanyPermission.create(name: "Edit Company Users")
-CompanyPermission.create(name: "Cancel Company Users")
+CompanyPermission.create(id: 1, name: "Invite Company Users")
+CompanyPermission.create(id: 2, name: "View Company Users")
+CompanyPermission.create(id: 3, name: "Edit Company Users")
+CompanyPermission.create(id: 4, name: "Cancel Company Users")
 
 # Company Roles Permissions
-CompanyPermission.create(name: "Create Company Roles")
-CompanyPermission.create(name: "View Company Roles")
-CompanyPermission.create(name: "Edit Company Roles")
-CompanyPermission.create(name: "Delete Company Roles")
+CompanyPermission.create(id: 5, name: "Create Company Roles")
+CompanyPermission.create(id: 6, name: "View Company Roles")
+CompanyPermission.create(id: 7, name: "Edit Company Roles")
+CompanyPermission.create(id: 8, name: "Delete Company Roles")
 
 # CRUD For Merchants Permissions
-CompanyPermission.create(name: "Create Merchants")
-CompanyPermission.create(name: "View Merchants")
-CompanyPermission.create(name: "Edit Merchants")
-CompanyPermission.create(name: "Delete Merchants")
+CompanyPermission.create(id: 9, name: "Create Merchants")
+CompanyPermission.create(id: 10, name: "View Merchants")
+CompanyPermission.create(id: 11, name: "Edit Merchants")
+CompanyPermission.create(id: 12, name: "Delete Merchants")
 
 # Merchant Users Permissions
-CompanyPermission.create(name: "Invite Merchant Users")
-CompanyPermission.create(name: "View Merchant Users")
-CompanyPermission.create(name: "Edit Merchant Users")
-CompanyPermission.create(name: "Cancel Merchant Users")
+CompanyPermission.create(id: 13, name: "Invite Merchant Users")
+CompanyPermission.create(id: 14, name: "View Merchant Users")
+CompanyPermission.create(id: 15, name: "Edit Merchant Users")
+CompanyPermission.create(id: 16, name: "Cancel Merchant Users")
 
 
 
 # Merchant Users Permissions
-MerchantPermission.create(name: "Invite Merchant Users")
-MerchantPermission.create(name: "View Merchant Users")
-MerchantPermission.create(name: "Edit Merchant Users")
-MerchantPermission.create(name: "Cancel Merchant Users")
+MerchantPermission.create(id: 17, name: "Invite Merchant Users")
+MerchantPermission.create(id: 18, name: "View Merchant Users")
+MerchantPermission.create(id: 19, name: "Edit Merchant Users")
+MerchantPermission.create(id: 20, name: "Cancel Merchant Users")
 
 # Merchant Roles Permissions
-MerchantPermission.create(name: "Create Merchant Roles")
-MerchantPermission.create(name: "View Merchant Roles")
-MerchantPermission.create(name: "Edit Merchant Roles")
-MerchantPermission.create(name: "Delete Merchant Roles")
+MerchantPermission.create(id: 21, name: "Create Merchant Roles")
+MerchantPermission.create(id: 22, name: "View Merchant Roles")
+MerchantPermission.create(id: 23, name: "Edit Merchant Roles")
+MerchantPermission.create(id: 24, name: "Delete Merchant Roles")
