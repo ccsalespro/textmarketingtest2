7c062cf7b01e75dede723949442d4f82c1d00f06
micahnico <micahnicodemus22@gmail.com>
2017-07-06 16:34:23 -0400

    try to fix permissions

--- app/services/company_role_permission_logic/create_company_role.rb
+++ app/services/company_role_permission_logic/create_company_role.rb
@@ -1,6 +1,6 @@
 class CreateCompanyRole < CompanyRolePermissionLogic
   def evaluate
-    if @permission_set.include?( CompanyPermission.find_by(name: "Create Company Roles") )
+    if @permission_set.include?( CompanyPermission.find_by(id: 5) )
        return true
    	else
    		redirect_to root_path
